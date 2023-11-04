e0d3529b78dbe08ac0624097893235d48c4d4e25
amir <amir.boisov@gmail.com>
2023-11-04 20:30:39 +0300

    changed the voice

--- core/functionality/tts.py
+++ core/functionality/tts.py
@@ -6,7 +6,7 @@ elevenlabs.set_api_key(ELEVEN_LABS)
 
 
 voice = elevenlabs.Voice(
-    voice_id="Rachel",
+    voice_id="Antony",
     settings=elevenlabs.VoiceSettings(
         stability=0.5,
         similarity_boost=0.6,
@@ -17,7 +17,7 @@ voice = elevenlabs.Voice(
 def generate_tts(from_gpt, chat_id):
     audio = elevenlabs.generate(    
     text=from_gpt,
-    voice="Rachel",
+    voice="Antony",
     model="eleven_multilingual_v2"
     )
     elevenlabs.save(audio, f"D:/Code/Python/kai_v1/voice_messages/generated{chat_id}.mp3")
